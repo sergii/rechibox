a34d2b0ad955f4ae3c836aab60f021b4ee47c280
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-12 03:35:50 +0300

    Fix React Native absolute fill typing

--- src/app/inventory.tsx
+++ src/app/inventory.tsx
@@ -476,7 +476,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   cameraPaused: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFill,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 24,
