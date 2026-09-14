ba1d99c470f2e6ded329fca8974ac27bd9d9f778
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-15 00:23:51 +0300

    Associate relational World State records

--- backend/app/models/world_document.rb
+++ backend/app/models/world_document.rb
@@ -1,2 +1,4 @@
 class WorldDocument < ApplicationRecord
+  has_many :world_entities, foreign_key: :world_id, dependent: :delete_all, inverse_of: :world_document
+  has_many :world_claims, foreign_key: :world_id, dependent: :delete_all, inverse_of: :world_document
 end
